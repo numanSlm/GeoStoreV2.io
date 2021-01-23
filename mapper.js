8e57dc03893eef48534e1074c0f9274d907ed1bf
Numan <numanslm99@gmail.com>
2021-01-23 11:42:10 +0530

    UserLoc display

--- mapper.js
+++ mapper.js
@@ -253,13 +253,28 @@ function geolocation(){
 
             localStorage.setItem('lat', lat);
             localStorage.setItem('long', long);
-
+            getReverseGeocodingData(lat,long);
             fillStoreList(lat, long);
         });
         
     }
   };
-
+  function getReverseGeocodingData(lat, lng) {
+    var latlng = new google.maps.LatLng(lat, lng);
+    // This is making the Geocode request
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({ 'latLng': latlng }, function (results, status) {
+        if (status !== google.maps.GeocoderStatus.OK) {
+            alert(status);
+        }
+        // This is checking to see if the Geocode Status is OK before proceeding
+        if (status == google.maps.GeocoderStatus.OK) {
+            //
+            var address = (results[0].formatted_address);
+            MyCurrentLoc.textContent = `Location:${address} `;
+        }
+    });
+}
 
 //store template render
 
